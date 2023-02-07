4e61c8169c99c1315138872d71a53bfb1ee0cdd3
edusantanaw <eduardosantana20122002@gmail.com>
2023-02-07 12:18:40 -0300

    feat: auth controller

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,12 +1,29 @@
+import { badRequest, server, success } from "../helpers/http-response";
 
 type data = {
     username: string;
     password: string;
 }
 
-export default function authController(){
-    return async ({username, password}:data) =>{
-        if(!username) return
-        if(!password) return
+type IUser = {
+    id: string
+    username: string;
+    password: string;
+}
+
+interface IAuth {
+    authUsecase: (username: string, password: string) => Promise<{ accessToken: string, user: IUser }>
+}
+
+export default function authController({ authUsecase }: IAuth) {
+    return async ({ username, password }: data) => {
+        try {
+            if (!username) return badRequest("O nome de usuario é necessario!")
+            if (!password) return badRequest("A senha é necessaria!")
+            const { accessToken, user } = await authUsecase(username, password)
+            return success({ accessToken, user })
+        } catch (error) {
+            return server(error)
+        }
     }
 }
